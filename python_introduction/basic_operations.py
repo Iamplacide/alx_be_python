e0d41b345ab15949db88a8799c339a4ea9aced6e
Placide <placidewiam@gmail.com>
2024-09-11 14:24:54 -0700

    task 1 basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#basic operation task in python which covers addition,subtraction andmultiplication
+
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+sub = number1 - number2
+prod = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", add)
+print("Subtraction of", number1, "and", number2, "is", sub)
+print("Multiplication of", number1, "and", number2, "is", prod)
