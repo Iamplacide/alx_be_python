a57ea454e9e63643d2e00c2eea602be0e8eb8e72
Placide <placidewiam@gmail.com>
2024-09-11 14:45:33 -0700

    The interest calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#Program that calculate a interest over a period of time
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print("The simple interest is : ", interest)
