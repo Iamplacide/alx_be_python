9fe7930ccd34f6eee7079f5be1433ef6473d3fb8
Placide <placidewiam@gmail.com>
2024-11-27 06:39:52 -0800

    arithmetics

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,8 @@
-def perform_operation ():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").lower()
-    
+def perform_opration(num1, num2, operation):
     match operation:
         case "add":
-            result = num1 + num2
+            return = num1 + num2
         case "multiply":
-            result = num1 * num2
+            return = num1 * num2
         case "divide":
-            result = num1 / num2
-            
-    print(f"Result: {result}")
-
-
+            return = num1 / num2
