83542503250a29db6439f6ca7e7c8d5547034d90
Placide <placidewiam@gmail.com>
2024-11-27 07:55:01 -0800

    return used correctly

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operation(num1, num2, operation):
-try :
     operand = operation.lower()
     match operand:
         case "add":
-            return = num1 + num2
+            return num1 + num2
         case "multiply":
-            return = num1 * num2
+            return num1 * num2
         case "divide":
-            return = num1 / num2
+            if num1 == 0 or num2 == 0:
+                return "Can't divide by zero"
+            else:
+                return num1 / num2
         case "subtract":
-            return = num1 - num2
-
-except ZeroDivisionError:
-    print("Can't divide by zero")
+            return num1 - num2
